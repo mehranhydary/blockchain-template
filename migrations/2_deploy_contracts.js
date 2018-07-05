6c3d1c8e223bc1a7bf4bb4cba58c90214fe009bc
Mehran Hydary <mehranhydary@gmail.com>
2018-07-05 19:05:41 -0400

    initial commit

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,9 @@
+/* 
+    FOLLOW THE FOLLOWING FORMAT TO IMPORT OTHER CONTRACTS: 
+    
+    var Identity = artifacts.require("./Identity.sol");
+
+    module.exports = function(deployer) {
+    deployer.deploy(Identity);
+    };
+*/
